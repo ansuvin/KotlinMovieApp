8d3c37ec5d3302a79592a4c8323a78294b072963
ansuvin <dkstnqls0925@naver.com>
2021-05-16 15:49:29 +0900

    [UPDATE] BottomNavigationView

--- app/src/main/java/com/example/kotlinmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmovieapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.kotlinmovieapp.ui.movie_list.MovieAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,9 +25,9 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_movie_list, R.id.navigation_movie_rank, R.id.navigation_fillmmaker))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
+        navView.selectedItemId = R.id.navigation_movie_rank
     }
 }
